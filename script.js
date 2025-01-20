0325d948ad4a317372de246f547e9bd11ab188ae
Atulp24 <apk200209@gmail.com>
2025-01-20 18:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 // complete this js code
 class Person {
-	constructor(name, age) {
-		this.name = name
-		this.age = age
+	constructor (name, age) {
+		this.name = name;
+		this.age = age;
 	}
-	greet() {
-		console.log(`Hello my name is ${this.name} and I am ${this.age} years old.`);
+	greet(){
+	console.log(
+		`Hello, my name is ${this.name} and I am ${this.age} years old.`
+	);
 	}
 }
-
-class Employee extends Person {
-	constructor(name, age, jobTitle) {
-		super(name, age)
-		this.jobTitle = jobTitle
+ 
+class Employee extends Person{
+	constructor(name,age,jobTitle){
+		super(name,age);
+		this.jobTitle = jobTitle;
 	}
-	jobGreet() {
-		console.log(`Hello my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+ 
+	jobGreet(){
+		console.log(
+			`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+		);
 	}
 }
 
